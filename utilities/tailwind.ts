2aec71897d1747912623eeac13910af3d8e8b6fb
Ajay Singh <ajay@nrb.org.np>
2024-08-13 10:54:22 +0545

    twmerge cn function fixed

--- utilities/tailwind.ts
+++ utilities/tailwind.ts
@@ -1,7 +1,6 @@
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { clsx, ClassValue } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
-  // Merge class names
   return twMerge(clsx(inputs));
 }

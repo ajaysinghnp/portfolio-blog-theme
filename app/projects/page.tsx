5061f119e0c12c7d91c2b85be96c8b3e076e2cd9
Ajay Singh <admin@ajaysingh.com.np>
2024-08-14 02:00:42 +0545

    ; else git add . && git commit -m "$1"; fi; }; f

---
+++ app/projects/page.tsx
@@ -0,0 +1,169 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Navigation } from "@/components/nav"; // Ensure you have the correct path for Navigation
+import { Card } from "@/components/card"; // Ensure you have the correct path for Card
+import { Article } from "./article"; // Ensure you have the correct path for Article
+import Link from "next/link"; // Assuming you are using Next.js
+import { Eye } from "lucide-react"; // Ensure you have the correct path for Eye
+
+export interface Project {
+  slug: string;
+  title: string;
+  description: string;
+  date: string;
+  published: boolean;
+}
+
+export default function ProjectsPage() {
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [views, setViews] = useState({});
+
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const { data } = await axios.get(
+          "https://api.github.com/users/mentormaya/repos"
+        );
+        const transformedProjects: Project[] = data.map((repo: any) => ({
+          slug: repo.full_name,
+          title: repo.name,
+          description: repo.description,
+          date: repo.created_at,
+          published: true,
+        }));
+
+        setProjects(transformedProjects);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    }
+
+    fetchProjects();
+  }, []);
+
+  if (projects.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  const featured = projects.find(
+    (project) => project.slug === "mentormaya/mentormaya.github.io"
+  )!;
+  const top2 = projects.find((project) => project.slug === "mentormaya/blog")!;
+  const top3 = projects.find(
+    (project) => project.slug === "mentormaya/addons-example"
+  )!;
+  const sorted = projects
+    .filter((p) => p.published)
+    .filter(
+      (project) =>
+        project.slug !== featured.slug &&
+        project.slug !== top2.slug &&
+        project.slug !== top3.slug
+    )
+    .sort(
+      (a, b) =>
+        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
+    );
+
+  return (
+    <div className="relative pb-16">
+      <Navigation />
+      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+            Projects
+          </h2>
+          <p className="mt-4 text-zinc-400">
+            Some of the projects are from work and some are on my own time.
+          </p>
+        </div>
+        <div className="w-full h-px bg-zinc-800" />
+
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
+          <Card>
+            <Link href={`/projects/${featured.slug}`}>
+              <article className="relative w-full h-full p-4 md:p-8">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="text-xs text-zinc-100">
+                    {featured.date ? (
+                      <time dateTime={new Date(featured.date).toISOString()}>
+                        {Intl.DateTimeFormat(undefined, {
+                          dateStyle: "medium",
+                        }).format(new Date(featured.date))}
+                      </time>
+                    ) : (
+                      <span>SOON</span>
+                    )}
+                  </div>
+                  <span className="flex items-center gap-1 text-xs text-zinc-500">
+                    <Eye className="w-4 h-4" />{" "}
+                    {Intl.NumberFormat("en-US", { notation: "compact" }).format(
+                      0
+                    )}
+                  </span>
+                </div>
+
+                <h2
+                  id="featured-post"
+                  className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                >
+                  {featured.title}
+                </h2>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  {featured.description}
+                </p>
+                <div className="absolute bottom-4 md:bottom-8">
+                  <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                    Read more <span aria-hidden="true">&rarr;</span>
+                  </p>
+                </div>
+              </article>
+            </Link>
+          </Card>
+
+          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+            {[top2, top3].map((project) => (
+              <Card key={project.slug}>
+                <Article project={project} views={0} />
+              </Card>
+            ))}
+          </div>
+        </div>
+        <div className="hidden w-full h-px md:block bg-zinc-800" />
+
+        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+          <div className="grid grid-cols-1 gap-4">
+            {sorted
+              .filter((_, i) => i % 3 === 0)
+              .map((project) => (
+                <Card key={project.slug}>
+                  <Article project={project} views={0} />
+                </Card>
+              ))}
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            {sorted
+              .filter((_, i) => i % 3 === 1)
+              .map((project) => (
+                <Card key={project.slug}>
+                  <Article project={project} views={0} />
+                </Card>
+              ))}
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            {sorted
+              .filter((_, i) => i % 3 === 2)
+              .map((project) => (
+                <Card key={project.slug}>
+                  <Article project={project} views={0} />
+                </Card>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

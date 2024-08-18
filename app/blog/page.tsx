a8f7400cdaa2c718ddb7bbab3aff272218258ae5
Ajay Singh <ajay@nrb.org.np>
2024-08-18 16:36:33 +0545

    folder structure and files created

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='text-white'>BlogPage</div>
+  )
+}
+
+export default BlogPage
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const navigation = [
   { name: "Projects", href: "/projects" },
   { name: "Blog", href: "/blog" },
   { name: "About", href: "/about" },
-  { name: "Contact", href: "/contacts" },
+  { name: "Contact", href: "/contact" },
 ];
 
 export default function Home() {

ef32278cdd9bbe281742eb58811436c25cd1a92e
Ajay Singh <admin@ajaysingh.com.np>
2024-08-21 00:42:26 +0545

    resume page completed

--- app/(root)/resume/page.tsx
+++ app/(root)/resume/page.tsx
@@ -1,13 +1,138 @@
+import PingingDot from "@/components/pinging-dot";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
+import { resume } from "@/data/resume";
+import { socialMedia } from "@/data/social";
+import authorImage from "@/public/images/author.png";
+import { BriefcaseBusiness, CircleUserRound, GraduationCap, PhoneCall, PocketKnife } from "lucide-react";
+import Link from "next/link";
+
 const ResumePage = () => {
   return (
     <main className="flex flex-1 gap-4">
-      <div className="w-[35%] max-w-[24rem] bg-zinc-900 rounded p-4">
-        <h1 className="text-2xl w-full text-center">
-          Ajay <span className="text-white">Singh</span>
-        </h1>
+      <div className="flex flex-col w-[35%] max-w-[24rem] items-center bg-zinc-900 rounded p-4 border border-zinc-600/60">
+        <section className="text-center">
+          <div className="mb-6">
+            <Avatar className="h-48 w-48 border-white border-4 bg-[radial-gradient(white,transparent)]">
+              <AvatarImage src={authorImage.src} alt="@ResumeAuthor" />
+              <AvatarFallback className="text-black">Ajay</AvatarFallback>
+            </Avatar>
+          </div>
+          <h1 className="text-2xl w-full text-center">
+            {resume.fname} <span className="text-white">{resume.lname}</span>
+          </h1>
+          <p className="mt-2">{resume.position}</p>
+        </section>
+        <Separator className="my-4 bg-zinc-600/60" />
+        <section className="p-2 flex flex-col gap-4 w-full">
+          <div className="section-header flex gap-4 items-center text-2xl">
+            <CircleUserRound className="h-8 w-8 text-purple-600" />
+            <h2 className="uppercase text-purple-600">Profile</h2>
+          </div>
+          <p className="text-justify text-muted">
+            {resume.profile}
+          </p>
+        </section>
+        <Separator className="my-4 bg-zinc-600/60" />
+        <section className="p-2 flex flex-col gap-4 w-full">
+          <div className="section-header flex gap-4 items-center text-2xl">
+            <PhoneCall className="h-8 w-8 text-purple-600" />
+            <h2 className="uppercase text-purple-600">Contact</h2>
+          </div>
+          <ul>
+            {resume.contact.map((contact, index) => {
+              return (
+                <li className="flex gap-4 items-center" key={index}>
+                  <PingingDot />
+                  {contact}
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+        <Separator className="my-4 bg-zinc-600/60" />
+        <section className="p-2 flex flex-col gap-4 w-full">
+          <div className="section-header flex gap-4 items-center text-2xl">
+            <PhoneCall className="h-8 w-8 text-purple-600" />
+            <h2 className="uppercase text-purple-600">Social Links</h2>
+          </div>
+          <ul>
+            {Object.entries(socialMedia).map(([key, value]) => (
+              <li key={key} className="flex gap-4 items-center">
+                <PingingDot />
+                <Link href={value.href} target="_blank" rel="noopener">{value.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </section>
       </div>
-      <div className="w-full bg-zinc-900 rounded p-4">Right Pannel</div>
-    </main>
+      <div className="w-full flex flex-col gap-4">
+        <section className="flex flex-col gap-4 bg-zinc-900 rounded p-6 border border-zinc-600/60">
+          <div className="section-header flex gap-4 items-center text-2xl">
+            <BriefcaseBusiness className="h-8 w-8 text-purple-600" />
+            <h2 className="uppercase">Work Experience</h2>
+          </div>
+          <ul className="flex flex-col ml-2">
+            {resume.workExperiences.map((work, index) => {
+              return (
+                <li className="flex items-center gap-8 relative py-4" key={index}>
+                  <div className="absolute h-full w-[2px] left-[5px] bg-slate-300/60"></div>
+                  <PingingDot />
+                  <div>
+                    <h3 className="uppercase text-purple-600">{work.title} <span className="capitalize ml-4 text-slate-300/60">{work.date}</span></h3>
+                    <p>{work.company}</p>
+                    <p className="text-slate-300/60">{work.description}</p>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
+        <section className="flex flex-col gap-4 bg-zinc-900 rounded p-6 border border-zinc-600/60">
+          <div className="section-header flex gap-4 items-center text-2xl">
+            <GraduationCap className="h-8 w-8 text-purple-600" />
+            <h2 className="uppercase">Education/Qualifications</h2>
+          </div>
+          <ul className="flex flex-col ml-2">
+            {resume.education.map((education, index) => {
+              return (
+                <li className="flex items-center gap-8 relative py-4" key={index}>
+                  <div className="absolute h-full w-[2px] left-[5px] bg-slate-300/60"></div>
+                  <PingingDot />
+                  <div>
+                    <h3 className="uppercase text-purple-600">{education.degree} <span className="capitalize ml-4 text-slate-300/60">{education.date}</span></h3>
+                    <p>{education.school}</p>
+                    <p className="text-slate-300/60">{education.description}</p>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
+        <section className="flex flex-col gap-4 bg-zinc-900 rounded p-6 border border-zinc-600/60">
+          <div className="section-header flex gap-4 items-center text-2xl">
+            <PocketKnife className="h-8 w-8 text-purple-600" />
+            <h2 className="uppercase text-purple-600">Skills</h2>
+          </div>
+          <div className="grid grid-cols-3 ml-2 gap-2">
+            {resume.skills.map((skill, index) => {
+              return (
+                <div className="flex flex-col justify-center rounded p-4 border border-zinc-600/60" key={index}>
+                  <div className="flex items-center gap-4">
+                    <span className="text-slate-300/60">{skill.percentage}%</span>
+                    <div className="w-full h-2 bg-slate-300/60 rounded-full">
+                      <div className="h-full bg-slate-300/80 rounded-full" style={{ width: `${skill.percentage}%` }}></div>
+                    </div>
+                  </div>
+                  <h3 className="uppercase text-purple-600">{skill.label}</h3>
+                  <p className="text-slate-300/60 text-justify">{skill.description}</p>
+                </div>
+              )
+            })}
+          </div>
+        </section>
+      </div >
+    </main >
   );
 };
 
---
+++ data/resume.ts
@@ -0,0 +1,81 @@
+export const resume = {
+  fname: "Ajay",
+  lname: "Singh",
+  name: "Ajay Singh",
+  position: "IT Assistant @NRB",
+  profile:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil corporis recusandae doloribus fugit nemo. Pariatur quis reiciendis ad reprehenderit cupiditate aperiam dicta magnam necessitatibus accusantium!",
+  contact: [
+    "+977 9846449912",
+    "admin@ajaysingh.com.np",
+    "Nepal Rastra Bank, Baluwatar KTM",
+  ],
+  workExperiences: [
+    {
+      title: "IT Assistant",
+      company: "Nepal Rastra Bank",
+      date: "[2021 - Present]",
+      description:
+        "Currently working as an IT Assistant at Nepal Rastra Bank. My responsibilities include managing the IT infrastructure, providing technical support to the staff, and ensuring the smooth operation of the IT systems.",
+    },
+  ],
+  education: [
+    {
+      degree: "Bachelor of Electronics and Communication Engineering",
+      school: "Purwanchal Campus, Dharan",
+      date: "[2017 - 2021]",
+      description:
+        "I completed my Bachelor's degree in Computer Engineering from Pulchowk Campus, Institute of Engineering, Tribhuvan University. I graduated in 2021 with a CGPA of 3.8/4.0.",
+    },
+    {
+      degree: "Diploma of Electronics and Communication Engineering",
+      school: "Paschimanchal Campus, Pokhara",
+      date: "[2017 - 2021]",
+      description:
+        "I completed my Bachelor's degree in Computer Engineering from Pulchowk Campus, Institute of Engineering, Tribhuvan University. I graduated in 2021 with a CGPA of 3.8/4.0.",
+    },
+    {
+      degree: "School Leaving Certificate (SLC)",
+      school: "Shree Janta Madhyamik Vidyalaya, Chandralalpur",
+      date: "[2017 - 2021]",
+      description:
+        "I completed my Bachelor's degree in Computer Engineering from Pulchowk Campus, Institute of Engineering, Tribhuvan University. I graduated in 2021 with a CGPA of 3.8/4.0.",
+    },
+  ],
+  skills: [
+    {
+      percentage: 95,
+      label: "Internet of Things (IoT)",
+      description:
+        "I have a strong understanding of IoT technologies and have worked on several projects involving IoT devices.",
+    },
+    {
+      percentage: 80,
+      label: "Software Development",
+      description:
+        "I have experience in developing software applications using various programming languages and frameworks.",
+    },
+    {
+      percentage: 75,
+      label: "DevOps",
+      description:
+        "I have experience in implementing DevOps practices and tools to automate software development and deployment processes.",
+    },
+    {
+      percentage: 70,
+      label: "Cybersecurity",
+      description:
+        "I have a good understanding of cybersecurity concepts and best practices.",
+    },
+    {
+      percentage: 40,
+      label: "Blockchain Technology",
+      description: "I have basic knowledge of blockchain technology.",
+    },
+    {
+      percentage: 40,
+      label: "Blockchain Technology",
+      description: "I have basic knowledge of blockchain technology.",
+    },
+  ],
+};

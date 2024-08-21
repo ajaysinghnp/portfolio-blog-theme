4828a8d735c128d1a00d6e8da1d23b4e805ef837
Ajay Singh <admin@ajaysingh.com.np>
2024-08-21 23:41:01 +0545

    projects rendering issue resolved

--- app/(root)/projects/[name]/page.tsx
+++ app/(root)/projects/[name]/page.tsx
@@ -1,7 +1,8 @@
 import "./mdx.css";
 import { Project } from "@/types/github";
-import { fetchProjects } from "@/lib/projects";
+import { fetchProjectReadme, fetchProjects } from "@/lib/projects";
 import ProjectPage from "./project";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 type Props = {
   params: {
@@ -16,9 +17,14 @@ export async function generateStaticParams(): Promise<Props["params"][]> {
 
 export default async function ProjectLoadingPage({ params }: Props) {
   const name = params?.name;
+  const readMe: string = await fetchProjectReadme(name);
+
   return (
     <main className="space-y-2">
       <ProjectPage project_name={name} />
+      <article className="px-4 py-12 mx-auto text-white">
+        <MDXRemote source={readMe} />
+      </article>
     </main>
   );
 }
--- app/(root)/projects/[name]/project.tsx
+++ app/(root)/projects/[name]/project.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { Header } from "./header";
-import { fetchProject, fetchProjectReadme } from "@/lib/projects";
+import { fetchProject } from "@/lib/projects";
 import { Project } from "@/types/github";
 import useSWR from "swr";
-import ReadMePage from "./readme";
 
 interface Props {
   project_name: string;
@@ -13,22 +12,17 @@ interface Props {
 const ProjectPage = ({ project_name }: Props) => {
   const {
     data: project,
-    error: d_error,
-    isLoading: d_isLoading,
+    error,
+    isLoading,
   } = useSWR<Project | null>(project_name, fetchProject);
-  const {
-    data: readMe,
-    error: r_error,
-    isLoading: r_isLoading,
-  } = useSWR<string | null>(`${project_name}-readme`, fetchProjectReadme);
 
-  if (d_error)
+  if (error)
     return (
       <div className="relative h-dvh flex justify-center items-center">
         Something happened while fetching project details...
       </div>
     );
-  if (d_isLoading)
+  if (isLoading)
     return (
       <div className="relative h-dvh flex justify-center items-center">
         Loading...
@@ -44,9 +38,6 @@ const ProjectPage = ({ project_name }: Props) => {
   return (
     <article>
       <Header project={project} views={5} />
-      <ReadMePage
-        readMe={readMe ? readMe : "# No Content Loaded yet! Please wait..."}
-      />
     </article>
   );
 };

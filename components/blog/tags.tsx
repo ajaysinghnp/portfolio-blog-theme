6bf22be2631ba1f4d16a969591e8269482adf792
Ajay Singh <ajay@nrb.org.np>
2024-08-28 15:41:08 +0545

    tags updated

--- components/blog/tags.tsx
+++ components/blog/tags.tsx
@@ -11,12 +11,12 @@ const TagsSection = () => {
         <Link
           className={cn(
             buttonVariants({ variant: "outline" }),
-            "bg-transparent pt-3 hover:bg-purple-500/60 hover:text-white transition duration-1000 relative"
+            "bg-transparent pt-3 hover:bg-purple-500/60 hover:text-white transition duration-1000 relative group"
           )}
           href={`/blog/tag/${tag.slug.toLocaleLowerCase() === "all" ? "" : tag.slug.toLocaleLowerCase()}`}
           key={tag.slug}
         >
-          {tag.label}{" ("}{tag.count}{")"}
+          {tag.label} <span className="pl-1 text-purple-500 group-hover:text-white duration-1000">{"{ "}{tag.count}{" }"}</span>
         </Link>
       ))}
     </div>

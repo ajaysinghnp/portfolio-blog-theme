e19a01dc7a79a97598717d68146a0ce56170086e
Ajay Singh <ajay@nrb.org.np>
2024-08-28 15:25:28 +0545

    project details page header updated

--- app/(root)/projects/[name]/header.tsx
+++ app/(root)/projects/[name]/header.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { socialMedia } from "@/data/social";
 import { fetchProject } from "@/lib/projects";
 import { Project } from "@/types/github";
 import { ArrowLeft, Eye, Github, Twitter } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import useSWR from "swr";
 
 interface Props {
@@ -25,13 +26,13 @@ const ProjectHeader = ({ project_name }: Props) => {
 
   if (project?.repository) {
     links.push({
-      label: "GitHub",
+      label: "Repository",
       href: `https://github.com/${project.repository}`,
     });
   }
   if (project?.url) {
     links.push({
-      label: "Website",
+      label: "Source Code",
       href: project.url,
     });
   }
@@ -65,82 +66,84 @@ const ProjectHeader = ({ project_name }: Props) => {
     );
 
   return (
-    <header
-      ref={ref}
-      className="relative isolate overflow-hidden bg-transparent"
-    >
-      <div
-        className={`fixed inset-x-0 top-12 z-50 backdrop-blur lg:backdrop-blur-none duration-200 border-b lg:bg-transparent ${isIntersecting
-          ? "bg-zinc-900/0 border-transparent"
-          : "bg-white/10  border-zinc-200 lg:border-transparent"
-          }`}
+    <Suspense fallback={<p>Loading {project_name}...</p>}>
+      <header
+        ref={ref}
+        className="relative isolate overflow-hidden bg-transparent"
       >
-        <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
-          <div className="flex justify-between gap-8">
-            <span
-              title="View counter for this page"
-              className={`duration-200 hover:font-medium flex items-center gap-1 ${isIntersecting
+        <div
+          className={`fixed inset-x-0 top-12 z-50 backdrop-blur lg:backdrop-blur-none duration-200 border-b lg:bg-transparent ${isIntersecting
+            ? "bg-zinc-900/0 border-transparent"
+            : "bg-white/10  border-zinc-200 lg:border-transparent"
+            }`}
+        >
+          <div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
+            <div className="flex justify-between gap-8">
+              <span
+                title="View counter for this page"
+                className={`duration-200 hover:font-medium flex items-center gap-1 ${isIntersecting
+                  ? " text-zinc-400 hover:text-zinc-100"
+                  : "text-zinc-600 hover:text-zinc-900"
+                  } `}
+              >
+                <Eye className="w-5 h-5" />{" "}
+                {Intl.NumberFormat("en-US", { notation: "compact" }).format(
+                  views,
+                )}
+              </span>
+              <Link target="_blank" href={socialMedia.twitter.href}>
+                <Twitter
+                  className={`w-6 h-6 duration-200 hover:font-medium ${isIntersecting
+                    ? " text-zinc-400 hover:text-zinc-100"
+                    : "text-zinc-600 hover:text-zinc-900"
+                    } `}
+                />
+              </Link>
+              <Link target="_blank" href={project.url}>
+                <Github
+                  className={`w-6 h-6 duration-200 hover:font-medium ${isIntersecting
+                    ? " text-zinc-400 hover:text-zinc-100"
+                    : "text-zinc-600 hover:text-zinc-900"
+                    } `}
+                />
+              </Link>
+            </div>
+
+            <Link
+              href="/projects"
+              className={`duration-200 hover:font-medium ${isIntersecting
                 ? " text-zinc-400 hover:text-zinc-100"
                 : "text-zinc-600 hover:text-zinc-900"
                 } `}
             >
-              <Eye className="w-5 h-5" />{" "}
-              {Intl.NumberFormat("en-US", { notation: "compact" }).format(
-                views,
-              )}
-            </span>
-            <Link target="_blank" href="https://twitter.com/chronark_">
-              <Twitter
-                className={`w-6 h-6 duration-200 hover:font-medium ${isIntersecting
-                  ? " text-zinc-400 hover:text-zinc-100"
-                  : "text-zinc-600 hover:text-zinc-900"
-                  } `}
-              />
-            </Link>
-            <Link target="_blank" href="https://github.com/chronark">
-              <Github
-                className={`w-6 h-6 duration-200 hover:font-medium ${isIntersecting
-                  ? " text-zinc-400 hover:text-zinc-100"
-                  : "text-zinc-600 hover:text-zinc-900"
-                  } `}
-              />
+              <ArrowLeft className="w-6 h-6 " />
             </Link>
           </div>
-
-          <Link
-            href="/projects"
-            className={`duration-200 hover:font-medium ${isIntersecting
-              ? " text-zinc-400 hover:text-zinc-100"
-              : "text-zinc-600 hover:text-zinc-900"
-              } `}
-          >
-            <ArrowLeft className="w-6 h-6 " />
-          </Link>
         </div>
-      </div>
-      <div className="container mx-auto relative isolate overflow-hidden  py-4 sm:py-12">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center">
-          <div className="mx-auto lg:mx-0">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-display">
-              {project.title.toLocaleUpperCase()}
-            </h1>
-            <p className="mt-6 text-lg text-muted-foreground sm:text-xl font-semibold leading-8">
-              {project.description}
-            </p>
-          </div>
+        <div className="container mx-auto relative isolate overflow-hidden">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center">
+            <div className="mx-auto lg:mx-0">
+              <h1 className="text-4xl font-bold tracking-normal text-white font-display">
+                {project.title.replaceAll("-", " ").toLocaleUpperCase()}
+              </h1>
+              <p className="mt-6 text-md text-muted-foreground line-clamp-2">
+                {project.description}
+              </p>
+            </div>
 
-          <div className="mx-auto mt-4 max-w-2xl lg:mx-0 lg:max-w-none">
-            <div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-              {links.map((link) => (
-                <Link target="_blank" key={link.label} href={link.href}>
-                  {link.label} <span aria-hidden="true">&rarr;</span>
-                </Link>
-              ))}
+            <div className="mx-auto mt-4 max-w-2xl lg:mx-0 lg:max-w-none">
+              <div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+                {links.map((link) => (
+                  <Link target="_blank" key={link.label} href={link.href}>
+                    {link.label} <span aria-hidden="true">&rarr;</span>
+                  </Link>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </Suspense>
   );
 };
 

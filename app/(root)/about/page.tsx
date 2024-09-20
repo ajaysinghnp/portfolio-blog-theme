d5644cbb2a30e7a34a3eccc81e21dade0a2170f3
Ajay Singh <admin@ajaysingh.com.np>
2024-09-20 23:43:28 +0545

    about page done

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,8 +1,47 @@
+import Education from '@/components/sections/education'
+import Experience from '@/components/sections/experience'
+import Skills from '@/components/sections/skills'
 import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div className='text-white'>AboutPage</div>
+    <main className='text-white rounded p-8 flex flex-col gap-8'>
+      <section className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold capitalize leading-[4rem] mb-4">I love creativity</h1>
+        <p className="text-2xl text-muted-foreground mb-4 text-center">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+        </p>
+        <div className="flex gap-8 text-justify text-muted-foreground leading-6 my-8">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, quos odio cumque perferendis obcaecati ratione expedita dolore tempore ullam omnis exercitationem adipisci deserunt vel quaerat sit excepturi porro minima ea dignissimos magni consequuntur perspiciatis quam? Fugiat cupiditate mollitia consectetur assumenda beatae saepe. Quidem debitis molestiae hic temporibus recusandae saepe esse, obcaecati sequi, dolores praesentium, autem nihil sit rem nemo quasi illum dolore vero. Neque maxime corrupti magnam, doloribus eveniet, beatae eius vero officiis deserunt animi soluta nemo labore provident quae officia iste ab, aliquid unde at laborum incidunt cumque maiores. Libero repellendus iure delectus accusamus doloribus fugit dolorem ducimus aut fugiat. Nam quos repellendus quaerat. Impedit autem amet temporibus minima eveniet deserunt maxime eligendi, quod aut incidunt. Nemo incidunt reiciendis perspiciatis voluptatem repellat. Quo, porro repellendus. Libero aliquam dicta in! Expedita, fuga. Aut, officia. Exercitationem, maiores deserunt illum, quis quae consectetur nobis hic ducimus aliquam nisi ullam. Inventore, doloribus fugit.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, quos odio cumque perferendis obcaecati ratione expedita dolore tempore ullam omnis exercitationem adipisci deserunt vel quaerat sit excepturi porro minima ea dignissimos magni consequuntur perspiciatis quam? Fugiat cupiditate mollitia consectetur assumenda beatae saepe. Quidem debitis molestiae hic temporibus recusandae saepe esse, obcaecati sequi, dolores praesentium, autem nihil sit rem nemo quasi illum dolore vero. Neque maxime corrupti magnam, doloribus eveniet, beatae eius vero officiis deserunt animi soluta nemo labore provident quae officia iste ab, aliquid unde at laborum incidunt cumque maiores. Libero repellendus iure delectus accusamus doloribus fugit dolorem ducimus aut fugiat. Nam quos repellendus quaerat. Impedit autem amet temporibus minima eveniet deserunt maxime eligendi, quod aut incidunt. Nemo incidunt reiciendis perspiciatis voluptatem repellat. Quo, porro repellendus. Libero aliquam dicta in! Expedita, fuga. Aut, officia. Exercitationem, maiores deserunt illum, quis quae consectetur nobis hic ducimus aliquam nisi ullam. Inventore, doloribus fugit.
+          </p>
+        </div>
+        <div className="flex my-8 px-8 w-full justify-between">
+          <div className="px-8 border-l border-purple-500">
+            <h2 className="text-3xl leading-10 mb-2">2020</h2>
+            <p className="text-md text-muted-foreground">Graduated in ECE</p>
+          </div>
+          <div className="px-8 border-l border-purple-500">
+            <h2 className="text-3xl leading-10 mb-2">6+ yrs</h2>
+            <p className="text-md text-muted-foreground">Experience in IT Field</p>
+          </div>
+          <div className="px-8 border-l border-purple-500">
+            <h2 className="text-3xl leading-10 mb-2">30+</h2>
+            <p className="text-md text-muted-foreground">Open Source Projects</p>
+          </div>
+          <div className="px-8 border-l border-purple-500">
+            <h2 className="text-3xl leading-10 mb-2">2012</h2>
+            <p className="text-md text-muted-foreground">Coding Since</p>
+          </div>
+        </div>
+      </section>
+      <Experience />
+      <Skills />
+      <Education />
+    </main>
   )
 }
 
--- app/(root)/resume/page.tsx
+++ app/(root)/resume/page.tsx
@@ -8,6 +8,9 @@ import { Separator } from "@/components/ui/separator";
 import { resume } from "@/data/resume";
 import { socialMedia } from "@/data/social";
 import authorImage from "@/public/images/author.png";
+import Experience from "@/components/sections/experience";
+import Education from "@/components/sections/education";
+import Skills from "@/components/sections/skills";
 
 const ResumePage = () => {
   return (
@@ -88,70 +91,9 @@ const ResumePage = () => {
         </section>
       </div>
       <div className="w-full flex flex-1 flex-col gap-4">
-        <section className="flex flex-col gap-4 bg-zinc-900/60 rounded p-6 border border-zinc-600/60">
-          <div className="section-header flex gap-4 items-center text-2xl">
-            <BriefcaseBusiness className="h-8 w-8 text-purple-600" />
-            <h2 className="uppercase">Work Experience</h2>
-          </div>
-          <ul className="flex flex-col ml-2">
-            {resume.workExperiences.map((work, index) => {
-              return (
-                <li className="flex items-center gap-8 relative py-4" key={index}>
-                  <div className="absolute h-full w-[2px] left-[5px] bg-slate-300/60"></div>
-                  <PingingDot />
-                  <div>
-                    <h3 className="uppercase text-purple-600">{work.title} <span className="capitalize ml-4 text-slate-300/60">{work.date}</span></h3>
-                    <p>{work.company}</p>
-                    <p className="text-slate-300/60">{work.description}</p>
-                  </div>
-                </li>
-              )
-            })}
-          </ul>
-        </section>
-        <section className="flex flex-col gap-4 bg-zinc-900/60 rounded p-6 border border-zinc-600/60">
-          <div className="section-header flex gap-4 items-center text-2xl">
-            <GraduationCap className="h-8 w-8 text-purple-600" />
-            <h2 className="uppercase">Education/Qualifications</h2>
-          </div>
-          <ul className="flex flex-col ml-2">
-            {resume.education.map((education, index) => {
-              return (
-                <li className="flex items-center gap-8 relative py-4" key={index}>
-                  <div className="absolute h-full w-[2px] left-[5px] bg-slate-300/60"></div>
-                  <PingingDot />
-                  <div>
-                    <h3 className="uppercase text-purple-600">{education.degree} <span className="capitalize ml-4 text-slate-300/60">{education.date}</span></h3>
-                    <p>{education.school}</p>
-                    <p className="text-slate-300/60">{education.description}</p>
-                  </div>
-                </li>
-              )
-            })}
-          </ul>
-        </section>
-        <section className="flex flex-col gap-4 bg-zinc-900/60 rounded p-6 border border-zinc-600/60">
-          <div className="section-header flex gap-4 items-center text-2xl">
-            <PocketKnife className="h-8 w-8 text-purple-600" />
-            <h2 className="uppercase text-purple-600">Skills</h2>
-          </div>
-          <div className="grid grid-cols-3 ml-2 gap-2">
-            {resume.skills.map((skill, index) => {
-              return (
-                <div className="flex flex-col justify-center rounded p-4 border border-zinc-600/60" key={index}>
-                  <div className="flex items-center gap-4">
-                    <span className="text-slate-300/60">{skill.percentage}%</span>
-                    <div className="w-full h-2 bg-slate-300/60 rounded-full">
-                      <div className="h-full bg-slate-300/80 rounded-full" style={{ width: `${skill.percentage}%` }}></div>
-                    </div>
-                  </div>
-                  <h3 className="uppercase text-purple-600">{skill.label}</h3>
-                  <p className="text-slate-300/60 text-justify">{skill.description}</p>
-                </div>
-              )
-            })}
-          </div>
-        </section>
+        <Experience />
+        <Education />
+        <Skills />
       </div >
     </main >
   );

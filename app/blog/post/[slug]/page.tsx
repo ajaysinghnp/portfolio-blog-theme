92feff52bf423f7a65d013427a36f92394334c9c
Ajay Singh <ajay@nrb.org.np>
2024-09-24 12:17:58 +0545

    final touch for the initial release

--- app/blog/post/[slug]/page.tsx
+++ app/blog/post/[slug]/page.tsx
@@ -15,21 +15,19 @@ const BlogPage = ({ params }: Props) => {
   const post = posts.find((post: Post) => post.slug === slug);
   return (
     <main className="flex flex-1 flex-col gap-4 p-4">
-      <h1 className="text-3xl tracking-wide leading-3">{post?.title}</h1>
-      <div className="flex justify-between items-center pr-4">
-        <p className="text-muted-foreground tracking-normal">{post?.excerpt}</p>
-        <div className="flex flex-row-reverse gap-1">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl tracking-wide leading-8">{post?.title}</h1>
+        <div className="flex gap-1 justify-end pt-1 px-1 rounded text-muted-foreground text-nowrap flex-wrap">
           <p className="text-muted-foreground tracking-normal">{calculateReadingTime(post ? post.content : "")}</p>
           {post?.tags.map(tag => (
-            <Link href={`/blog/tag/${slugify(tag)}`} key={tag}>
-              <div className="w-full h-full flex gap-1 justify-between items-center pt-1 border border-slate-600/60 hover:bg-purple-500/60 hover:text-white px-1 rounded text-muted-foreground transition duration-1000" key={tag}>
-                <Tag className="h-3 w-3" />
-                <span className="leading-4 text-sm">{tag}</span>
-              </div>
+            <Link href={`/blog/tag/${slugify(tag)}`} key={tag} className="flex gap-1 p-1 hover:text-white transition duration-1000">
+              <Tag className="h-3 w-3" />
+              <span className="leading-4 text-sm">{tag}</span>
             </Link>
           ))}
         </div>
       </div>
+      <p className="text-muted-foreground tracking-normal">{post?.excerpt}</p>
       <MDX source={post?.content} />
     </main>
   );
